e788415cc143f8fec52dd5890920e750d2b33bea
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-05 21:35:49 +0900

    feat: add rule metadata

--- enforce-foo-bar.js
+++ enforce-foo-bar.js
@@ -1,5 +1,15 @@
 module.exports = {
-  meta: {},
+  meta: {
+    type: "problem", // "problem" | "suggestion" | "layout"
+    docs: {
+      description:
+        "'foo'라는 변수 이름을 가진 변수들은 오직 'bar'라는 값만 갖도록 변환한다.",
+      recommended: true,
+      url: "https://eslint.org/docs/latest/extend/custom-rules#rule-structure",
+    },
+    fixable: "code",
+    schema: [],
+  },
   create(context) {
     return {};
   },
