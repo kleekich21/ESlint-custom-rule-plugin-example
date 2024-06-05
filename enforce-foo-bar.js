1cb939faf10495331a60c5af0bfd7fdf5ccfd0f4
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-05 21:38:01 +0900

    chore: setup for test

--- enforce-foo-bar.js
+++ enforce-foo-bar.js
@@ -11,7 +11,31 @@ module.exports = {
     schema: [],
   },
   create(context) {
-    return {};
+    return {
+      VariableDeclarator(node) {
+        if (node.parent.kind === "const") {
+          if (node.id.type === "Identifier" && node.id.name === "foo") {
+            if (
+              node.init &&
+              node.init.type === "Literal" &&
+              node.init.value !== "bar"
+            ) {
+              context.report({
+                node,
+                message:
+                  "'const foo'에 'bar'가 아닌 값이 할당되었습니다. 작업이 필요한 값: {{ notBar }}.",
+                data: {
+                  notBar: node.init.value,
+                },
+                fix(fixer) {
+                  return fixer.replace.Text(node.init, '"bar"');
+                },
+              });
+            }
+          }
+        }
+      },
+    };
   },
 };
 
